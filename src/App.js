a148896019424d83438d20122155882b24c16f93
jenishbabariya22 <jenishbabariya58@gmail.com>
2023-12-14 16:42:08 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import Record from './Record';
+import { useEffect, useState } from 'react';
 const App = () => {
-
-  const record = [
+  const [allrecord, setAllRecord] = useState([
     {
       id: 1,
       name: "samsung",
       price: 1000,
       qty: 1,
       category: "mobile",
-      color: "lightgreen"
+      img: "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcSGrWLGFZBjHHAFlcd2sSvwEX9qbKTWyRMyaK_xaQj8HpIuuAzmFPIgl_XOFapgzVUku3acrTBd9Bot3OWt0EFS-9EYIpxXOoc0OyJShPRX2b_QJ38ZP-WOpRvG0dmxTWOGN5-TuVZPqdU6&usqp=CAc",
+      color: "#883aeb",
+      text: "white"
     },
     {
       id: 2,
@@ -18,7 +20,8 @@ const App = () => {
       price: 500,
       qty: 1,
       category: "electronics",
-      color: "lightpink"
+      img: "https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcTRoe2KfNJo2lLlKW4dLdQ5Xzez3R42vPXjc3d-lAJLnmajss1FZCmaj-rr7q3NfeY2n5v0AcZ1qBjwQ_EtH_uI3fPpshwYAcjIaLF2rGL3RePVZxk0o4KieVdFEZIHQgWpmmwpSXtqZg&usqp=CAc",
+      color: "#fff"
     },
     {
       id: 3,
@@ -26,7 +29,9 @@ const App = () => {
       price: 250,
       qty: 1,
       category: "electronics",
-      color: "lightgreen"
+      img: "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcTcllUCjOu-eL1Q26TvMrPB-a6xsK_K5j3foF1xRGHGjEArlSD8Mv971iLEE0yOObiHVycbUGqXbnUfujAIc7Hbl62z6nzuA3Mx0JmopOyJ61gQ6CWn98Hx",
+      color: "#883aeb",
+      text: "white"
     },
     {
       id: 4,
@@ -34,7 +39,8 @@ const App = () => {
       price: 100,
       qty: 1,
       category: "mobile",
-      color: "lightpink"
+      img: "https://images.pexels.com/photos/1542252/pexels-photo-1542252.jpeg?auto=compress&cs=tinysrgb&w=600",
+      color: "#fff"
     },
     {
       id: 5,
@@ -42,7 +48,9 @@ const App = () => {
       price: 50,
       qty: 1,
       category: "cloth",
-      color: "lightgreen"
+      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSuQQ3Mewjwz9QYRvlMVeYnE6Lr2WklARqFLA&usqp=CAU",
+      color: "#883aeb",
+      text: "white"
     },
     {
       id: 6,
@@ -50,7 +58,8 @@ const App = () => {
       price: 20,
       qty: 1,
       category: "cloth",
-      color: "lightpink"
+      img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS6eLmlij1dMcLuiGznWiNSTBgC3VL-DQhDnQ&usqp=CAU',
+      color: "#fff"
     },
     {
       id: 7,
@@ -58,7 +67,9 @@ const App = () => {
       price: 400,
       qty: 1,
       category: "mobile",
-      color: "lightgreen"
+      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmJHwX0nSfPZEu6Pnc0ecbqs5J035ZhJEpuw&usqp=CAU",
+      color: "#883aeb",
+      text: "white"
     },
     {
       id: 8,
@@ -66,7 +77,8 @@ const App = () => {
       price: 150,
       qty: 1,
       category: "food",
-      color: "lightpink"
+      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_oYtFzhSw5j_sGbHdosqvS2kNmTLpJ8sRNA&usqp=CAU",
+      color: "#fff"
     },
     {
       id: 9,
@@ -74,19 +86,35 @@ const App = () => {
       price: 100,
       qty: 1,
       category: "food",
-      color: "lightgreen"
+      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTmD7sd_BbkgvPVUAnuHgBAqRvKcNcyoafYog&usqp=CAU",
+      color: "#883aeb",
+      text: "white"
     },
-  ];
+  ]);
 
+  const [filter, setFilter] = useState([]);
 
+  const handleClick = (category) => {
+    let oldrecord = [...allrecord];
 
+    if (category === "all") {
+      setFilter(allrecord)
+    } else {
+      oldrecord = allrecord.filter((item) => {
+        return item.category === category
+      })
+      setFilter(oldrecord)
+    }
+  }
 
-
-
+  useEffect(() => {
+    setFilter(allrecord)
+  }, [])
   return (
     <>
       <Record
-        allrecord={record}
+        allrecorddata={filter}
+        btnclick={handleClick}
       />
     </>
   )
--- src/Record.jsx
+++ src/Record.jsx
@@ -1,126 +1,26 @@
 import React, { useState } from 'react'
 
 
-function Record({ allrecord }) {
+function Record({ allrecorddata, btnclick }) {
 
-  const [record, setRecord] = useState([]);
-
-
-  const mobile = () => {
-    let ans = allrecord.filter((val) => {
-      return val.category === 'mobile'
-    })
-    {
-      setRecord(ans)
-      ans.map((item) => {
-        return (
-          <tr key={item.id}>
-            <td>{item.id}</td>
-            <td>{item.name}</td>
-            <td>{item.price}</td>
-            <td>{item.qty}</td>
-            <td>{item.category}</td>
-          </tr>
-        )
-      })
-    }
-    console.log(ans)
-  }
-  const electronic = () => {
-    let ans = allrecord.filter((val) => {
-      return val.category === 'electronics'
-    })
-    {
-      setRecord(ans)
-      ans.map((item) => {
-        return (
-          <tr key={item.id}>
-            <td>{item.id}</td>
-            <td>{item.name}</td>
-            <td>{item.price}</td>
-            <td>{item.qty}</td>
-            <td>{item.category}</td>
-          </tr>
-        )
-      })
-    }
-    console.log(ans)
-  }
-  const cloth = () => {
-    let ans = allrecord.filter((val) => {
-      return val.category === 'cloth'
-    })
-    {
-      setRecord(ans)
-      ans.map((item) => {
-        return (
-          <tr key={item.id}>
-            <td>{item.id}</td>
-            <td>{item.name}</td>
-            <td>{item.price}</td>
-            <td>{item.qty}</td>
-            <td>{item.category}</td>
-          </tr>
-        )
-      })
-    }
-    console.log(ans)
-  }
-  const food = () => {
-    let ans = allrecord.filter((val) => {
-      return val.category === 'food'
-    })
-    {
-      setRecord(ans)
-      ans.map((item) => {
-        return (
-          <tr key={item.id}>
-            <td>{item.id}</td>
-            <td>{item.name}</td>
-            <td>{item.price}</td>
-            <td>{item.qty}</td>
-            <td>{item.category}</td>
-          </tr>
-        )
-      })
-    }
-    console.log(ans)
-  }
-  const all = () => {
-    let ans = allrecord.filter((val) => {
-      return val
-    })
-    {
-      setRecord(ans)
-      ans.map((item) => {
-        return (
-          <tr key={item.id}>
-            <td>{item.id}</td>
-            <td>{item.name}</td>
-            <td>{item.price}</td>
-            <td>{item.qty}</td>
-            <td>{item.category}</td>
-          </tr>
-        )
-      })
-    }
-  }
-
-
+  const category = ["mobile", "electronics", "cloth", "food", "all"];
   return (
     <center>
       <h1>All Record</h1>
-      <button type="button" className="btn btn-primary" onClick={mobile} style={{ margin: "20px" }}>mobile</button>
-      <button type="button" className="btn btn-secondary" onClick={electronic} style={{ margin: "20px" }}>electronics</button>
-      <button type="button" className="btn btn-success" onClick={cloth} style={{ margin: "20px" }}>cloth</button>
-      <button type="button" className="btn btn-danger" onClick={food} style={{ margin: "20px" }}>food</button>
-      <button type="button" className="btn btn-warning" onClick={all} style={{ margin: "20px" }}>all</button>
+      {
+        category.map((cat, i) => {
+          return (
+            <button type="button" className="btn btn-primary" style={{ margin: "20px" }} onClick={() => btnclick(cat)} key={i}>{cat}</button>
+          )
+        })
+      }
       <br></br><br></br>
       <table className='table' >
         <thead >
           <tr key={1}  >
             <th>Id</th>
             <th>Name</th>
+            <th>Images</th>
             <th>Price</th>
             <th>Qty</th>
             <th>Category</th>
@@ -128,14 +28,15 @@ function Record({ allrecord }) {
         </thead>
         <tbody>
           {
-            record.map((item) => {
+            allrecorddata.map((item) => {
               return (
-                <tr key={item.id} >
-                  <td style={{ background: item.color }} >{item.id}</td>
-                  <td style={{ background: item.color }}>{item.name}</td>
-                  <td style={{ background: item.color }}>{item.price}</td>
-                  <td style={{ background: item.color }}>{item.qty}</td>
-                  <td style={{ background: item.color }}>{item.category}</td>
+                <tr key={item.id} style={{ fontWeight: "500", fontSize: "23px" }} >
+                  <td style={{ background: item.color, color: item.text }} >{item.id}</td>
+                  <td style={{ background: item.color, color: item.text }}>{item.name}</td>
+                  <td style={{ background: item.color, color: item.text }} ><img src={item.img} style={{ objectFit: "contain", height: "100px", width: "200px" }} /></td>
+                  <td style={{ background: item.color, color: item.text }}>{item.price}</td>
+                  <td style={{ background: item.color, color: item.text }}>{item.qty}</td>
+                  <td style={{ background: item.color, color: item.text }}>{item.category}</td>
                 </tr>
               )
             })
